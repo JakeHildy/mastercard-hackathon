e6b102d3b5873eab99c94f9715fff7d9627d0056
heather-ross <hross299@yahoo.com>
2021-04-30 13:53:26 -0400

    payment card page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './HomePage.scss';
-import Modal from './../../components/atoms/Modal/Modal';
+// import Modal from './../../components/atoms/Modal/Modal';
+import PaymentCard from '../../components/atoms/PaymentCard/PaymentCard';
 
 export class HomePage extends Component {
   state = {
@@ -8,42 +9,44 @@ export class HomePage extends Component {
     showCard: false,
   };
 
-  handleBuyNowPressed = (e) => {
-    e.preventDefault();
-    this.setState({ showCard: true });
-  };
+  // handleBuyNowPressed = (e) => {
+  //   e.preventDefault();
+  //   this.setState({ showCard: true });
+  // };
 
   handleVerificationClicked = (e) => {
-    console.log('Face Id Clicked');
+    console.log('Verify Payment Clicked');
+    e.preventDefault();
+    this.setState({ showCard: true });
   };
 
   handleFaceIdClicked = (e) => {
     console.log('Face Id Clicked');
   };
 
-  handleCloseModal = (e) => {
-    console.log('Close Modal');
-    this.setState({ showModal: false });
-  };
+  // handleCloseModal = (e) => {
+  //   console.log('Close Modal');
+  //   this.setState({ showModal: false });
+  // };
 
   render() {
     return (
       <div className="home-page">
         <button
           className="home-page__button"
-          onClick={this.handleBuyNowPressed}
+          onClick={this.handleVerificationClicked}
         >
           Buy now
         </button>
-        {this.state.showModal && (
-          <Modal
+        {this.state.showCard && (
+          <PaymentCard
             handleFaceIdClick={this.handleFaceIdClicked}
-            handleClose={this.handleCloseModal}
+            // handleClose={this.handleCloseModal}
           />
         )}
-        {this.state.showCard && (
+        {/* {this.state.showCard && (
           <PaymentCard handleVerification={this.handleVerificationClicked} />
-        )}
+        )} */}
       </div>
     );
   }
