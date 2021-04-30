0109e3da87c2b1cd09e3dc98787c78d84f58b8d7
ajit-mathew-joseph <65310049+ajit-mathew-joseph@users.noreply.github.com>
2021-04-30 10:45:01 -0700

    Added verification and failed pages. Added animations to showcase demo success and failed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import NavBar from './components/molecules/NavBar/NavBar';
 import Footer from './components/molecules/Footer/Footer';
 import HomePage from './pages/HomePage/HomePage';
 import SecondPage from './pages/SecondPage/SecondPage';
+import VerificationSuccess from "./pages/VerificationSuccess/VerificationSuccess";
+import VerificationFailed from "./pages/VerificationFailed/VerificationFailed";
 
 class App extends Component {
   render() {
@@ -26,6 +28,16 @@ class App extends Component {
               <Route
                 path="/second-page"
                 render={(routerProps) => <SecondPage {...routerProps} />}
+              />
+
+              <Route
+                path="/success"
+                render={(routerProps) => <VerificationSuccess {...routerProps} />}
+              />
+
+              <Route
+                path="/failed"
+                render={(routerProps) => <VerificationFailed {...routerProps} />}
               />
               {/* <Redirect from="/" to="/home" /> */}
             </Switch>
