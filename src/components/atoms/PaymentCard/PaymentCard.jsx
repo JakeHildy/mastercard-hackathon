2a636170b0daadc5c7884f556ef0b10d163f1eef
Jacob Hildebrandt <jake@hildy.ca>
2021-05-13 08:04:47 -0700

    Added Redirects folder to fix refresh issue

--- src/components/atoms/PaymentCard/PaymentCard.jsx
+++ src/components/atoms/PaymentCard/PaymentCard.jsx
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom';
 import './PaymentCard.scss';
 import card from '../../../assets/images/Card.png';
 
-function PaymentCard({handleFaceIdClick}) {
-    return (
-        <div className="payment-card">
+function PaymentCard({ handleFaceIdClick }) {
+  return (
+    <div className="payment-card">
       <div className="payment-card__popup">
         <Link
           to="/second-page"
           className="payment-card__popup-open"
           onClick={handleFaceIdClick}
         >
-            <button className="payment-card__button"></button>
+          <button className="payment-card__button"></button>
         </Link>
         <img className="payment-card__card" src={card} />
-        </div>
-        
+      </div>
     </div>
   );
 }
 
-export default PaymentCard
+export default PaymentCard;
